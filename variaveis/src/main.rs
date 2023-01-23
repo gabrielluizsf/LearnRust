455bd56a7cb6a25de705aa51209e3332929c61a8
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-23 18:46:07 -0300

    Variáveis

---
+++ variaveis/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let name            = "Gabriel";
+    let github_url: &str = "https://github.com/gabrielluizsf";
+
+    let mut age = 42;
+
+    age = 21;
+    
+    println!("Hello, {}!",name);
+    println!("Github: {}",github_url);
+    println!("Age: {}", age);
+}
