73fad786aada33b6020d5c716462e246c9871f5c
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-23 18:55:06 -0300

    Tipos de dados

---
+++ tipos/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //inumeros_de_bits
+    //i64 = 64 bits
+    //u64 = não permite números negativos
+    let number: u64 = 23; //i64
+    //f32 = 32 bits para float
+    let numero_flutuante: f64 = 20.50;
+    //booleano == bool
+    let booleano: bool = true;
+
+    println!("{},{},{}",number,numero_flutuante,booleano);
+}
