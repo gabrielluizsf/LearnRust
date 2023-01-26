5471764adf19bf578abd0d6634fabf9513b1b597
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-26 17:25:47 -0300

    Display

---
+++ display/src/main.rs
@@ -0,0 +1,51 @@
+use std::fmt; // Import `fmt`
+
+// A structure holding two numbers. `Debug` will be derived so the results can
+// be contrasted with `Display`.
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+// Implement `Display` for `MinMax`.
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+// Define a structure where the fields are nameable for comparison.
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+// Similarly, implement `Display` for `Point2D`
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+fn main() {
+
+    let minmax = MinMax(0, 14);
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range =   MinMax(-700, 700);
+    let small_range = MinMax(-7, 7);
+
+    println!("The big range is {big} and the small is {small}",
+             small = small_range,
+             big = big_range
+            );
+
+    let point = Point2D { x: 7.7, y: 7.2 };
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+    
+}
