855d3793b756bdad4baa6ead2d2fb8a77ca285f3
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-05 13:02:46 -0300

    Revendo conceito com o curso aprenda rust
    playlist: https://www.youtube.com/playlist\?list\=PLjSf4DcGBdiGCNOrCoFgtj0KrUq1MRUME

--- variaveis/src/main.rs
+++ variaveis/src/main.rs
@@ -1,12 +1,36 @@
+const SECONDS_IN_MINUTE: u32 = 60;
+const MINUTES_IN_HOUR: u32 = 60;
+const SECONDS_IN_HOUR: u32 = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+
 fn main() {
-    let name            = "Gabriel";
+    let name = "Gabriel";
     let github_url: &str = "https://github.com/gabrielluizsf";
 
-    let mut age = 42;
+    let year = 2022;
+    let age = 21;
+    print_info(age, year, name, github_url);
+    {
+        let age = 22;
+        let year = 2023;
+        print_info(age, year, name, github_url);
+    }
+    {
+        let age = 23;
+        let year = 2024;
+        print_info(age, year, name, github_url);
+    }
+    let total_hours: u32 = 30;
+    print_hours_in_seconds(total_hours)
+}
 
-    age = 21;
-    
-    println!("Hello, {}!",name);
-    println!("Github: {}",github_url);
+fn print_hours_in_seconds(total_hours: u32){
+    let hours_in_seconds = total_hours * SECONDS_IN_HOUR;
+    println!("Hours in seconnds: {}", hours_in_seconds);
+}
+
+fn print_info(age: i32, year: i32, name: &str, github_url: &str) {
+    println!("Year: {}", year);
+    println!("Hello, {}!", name);
+    println!("Github: {}", github_url);
     println!("Age: {}", age);
 }
